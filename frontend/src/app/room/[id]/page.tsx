75d00c5ba332489774e9dbd4c37f3077bf23d332
pkfire13 <pkim31@gmail.com>
2024-01-11 20:27:25 -0600

    rooms

--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
 export default function Room() {
+  const { id } = useParams();
+
   return (
     <>
-      <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <div>{id} Room</div>
+      </div>
     </>
   );
 }
